93d498a5678ea5e9d1de86ab37780d80c5fce70c
icuthy <icuthy@gmail.com>
2015-05-18 12:36:31 -0700

    solutions to codewars_8_NumbersToStrings and 8_StingsToNumbers

---
+++ codewars_8_NumbersToStrings.js
@@ -0,0 +1,19 @@
+// Convert a Number to a String:
+
+function numberToString(num) {
+  return num.toString();
+}
+
+//or
+
+function numberToString(num) {
+  return String(num);
+}
+
+//or
+
+function numberToString(num) {
+  return ''+num;
+}
+
+
---
+++ codewars_8_StingsToNumbers.js
@@ -0,0 +1,17 @@
+// Convert a String to a Number:
+
+var stringToNumber = function(str){
+  return parseInt(str);
+}
+
+//or
+
+var stringToNumber = function(str){
+  return Number(str);
+}
+
+//or
+
+var stringToNumber = function(str){
+  return +str;
+}
