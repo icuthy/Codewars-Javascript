497fe6e0568504acaba6c188a3498cea27049a75
icuthy <icuthy@gmail.com>
2015-06-02 11:52:12 -0700

    solution to codewars_7_ColorGhost

---
+++ Codewars_challenges/codewars_7_ColorGhost.js
@@ -0,0 +1,19 @@
+/*
+Color Ghost
+
+Create a class Ghost
+
+Ghost objects are instantiated without any arguments.
+
+Ghost objects are given a random color attribute of white" or "yellow" or "purple" or "red" when instantiated
+
+ghost = new Ghost();
+ghost.color //=> "white" or "yellow" or "purple" or "red"
+*/
+
+var Ghost = function() {
+ var colors = ["white", "yellow", "purple", "red"];
+  this.color = colors[Math.floor(colors.length * Math.random())];
+};
+
+var casper = new Ghost;
