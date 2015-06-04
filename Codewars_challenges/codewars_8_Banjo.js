03b98457573a0724745a6c9bfd8eacc342a31614
icuthy <icuthy@gmail.com>
2015-06-03 17:15:57 -0700

    solution to codewars_8_Banjo

---
+++ Codewars_challenges/codewars_8_Banjo.js
@@ -0,0 +1,13 @@
+/*
+Create a function which answers the question "Are you playing Banjo?". If your name starts with the letter "R" or lower case "r", you are playing Banjo!
+
+The function takes a name as its only argument, and returns one of the following strings:
+
+"[name] plays banjo"
+"[name] does not play banjo"
+Names given are always valid strings.
+*/
+
+function areYouPlayingBanjo(name) {
+  return name + (name[0].toLowerCase() === 'r' ? ' plays banjo' : ' does not play banjo');
+}
