e48f3eda4f389680f47e04bde74d5b8c06cd2a32
icuthy <icuthy@gmail.com>
2015-06-27 15:00:49 -0700

    solution to codewars_7_TeaForTwo

---
+++ Codewars_challenges/codewars_7_TeaForTwo.js
@@ -0,0 +1,35 @@
+/*
+Tea for two!
+
+Given a string or number write a function tea42 that takes as input a string or number and returns a string where every '2' charcter or digit has been replaced with a t.
+
+tea42('coffee') //should return 'coffee'
+tea42('tea') //should return 'tea'
+tea42('2ea') //should return 'tea'
+tea42(9022) //should return '90tt'
+tea42(5676765) //should return '5676765'
+tea42('2u2u') //should return 'tutu'
+*/
+
+function tea42(input) {
+input = input.toString();
+var result = input.replace(/2/g, "t");  
+return result;
+}
+
+
+// or
+
+function tea42(input) {
+input = input.toString();
+var result = '';
+for (var i = 0, len = input.length; i < len; i++) {
+    result += input[i] == "2" ? "t": input[i];
+}
+    return result;
+}
+
+// better yet:
+// function tea42(input) {
+//   return input.toString().replace(/2/g, 't')
+// }
