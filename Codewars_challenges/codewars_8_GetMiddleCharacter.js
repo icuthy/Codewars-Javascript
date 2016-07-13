0b2ea1863a852bbcb1e83b00745f0c1a5477e169
Ingrid Cuthbert <ingrid@efelle.com>
2016-07-13 14:03:58 -0700

    ^ codewars_8_GetMiddleCharacter.js

---
+++ Codewars_challenges/codewars_8_GetMiddleCharacter.js
@@ -0,0 +1,30 @@
+//*****************************************************************************************//
+// You are going to be given a word. Your job is to return the middle character of the word. 
+// If the word's length is odd, return the middle character. 
+// If the word's length is even, return the middle 2 characters.
+
+// Examples:
+
+// Kata.getMiddle("test") should return "es"
+
+// Kata.getMiddle("testing") should return "t"
+
+// Kata.getMiddle("middle") should return "dd"
+
+// Kata.getMiddle("A") should return "A"
+// Input
+
+// A word (string) of length 0 < str < 1000
+
+// Output
+//
+// The middle character(s) of the word represented as a string.
+//
+//*****************************************************************************************//
+
+
+
+function getMiddle(s) {
+  var x = s.length/2;
+  return s.length % 2 == 0 ? s.charAt(x - 1) + s.charAt(x): s.charAt(Math.floor(x));
+}
