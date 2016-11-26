b961e5ed14a3f0e98cd44564ff0a51a509b46f48
icuthy <icuthy@gmail.com>
2016-11-25 18:27:52 -0800

    Solution to codewars_8_EvenOrOdd.js

---
+++ Codewars_challenges/codewars_8_EvenOrOdd.js
@@ -0,0 +1,11 @@
+//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+function even_or_odd(number) {
+ return number % 2 == 0 ? "Even" : "Odd";
+}
+
+//Test.assertEquals(even_or_odd(2), "Even")
+// Test.assertEquals(even_or_odd(0), "Even")
+// Test.assertEquals(even_or_odd(7), "Odd")
+// Test.assertEquals(even_or_odd(1), "Odd")
